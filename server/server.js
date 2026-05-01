c9c60b9714aa9245b3d12956275bba60ca0d88a5
Arsh khan <2k22.csai.32029@gmail.com>
2026-05-01 09:33:53 +0530

    Fix Express 5 wildcard route crash

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use("/api/users", userRoutes);
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
